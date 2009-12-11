5cb33e52e8de1ca16bc5670c9a2e13103447c769
Matchu <matchu1993@gmail.com>
2009-12-11 16:07:32 -0500

    staff secret delete now requires confirmation

--- www/assets/staff.js
+++ www/assets/staff.js
@@ -1,17 +1,19 @@
 $(function () {
   $('.secret form').submit(function (e) {
     e.preventDefault();
-    var el = $(this);
-    $.ajax({
-      url: el.attr('action'),
-      data: el.serialize(),
-      type: 'post',
-      success: function () {
-        el.closest('.secret').fadeOut(1000);
-      },
-      error: function (data) {
-        alert("Error deleting: " + data);
-      }
-    })
+    if(confirm('Are you sure?')) {
+      var el = $(this);
+      $.ajax({
+        url: el.attr('action'),
+        data: el.serialize(),
+        type: 'post',
+        success: function () {
+          el.closest('.secret').fadeOut(1000);
+        },
+        error: function (data) {
+          alert("Error deleting: " + data);
+        }
+      })
+    }
   });
 });
