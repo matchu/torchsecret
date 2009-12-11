976436f22e9d46f3e0dc1c5a249e12ee62a3d196
Matchu <matchu1993@gmail.com>
2009-12-11 16:58:24 -0500

    added clicky stat counter

--- www/index.php
+++ www/index.php
@@ -73,5 +73,9 @@ $status_message = $status_messages[$_GET['status']];
       considered a falsehood and immediately deleted.
     </p>
   </footer>
+  <script src="http://static.getclicky.com/js" type="text/javascript"></script>
+  <script type="text/javascript">clicky.init(130633);</script>
+  <noscript><p><img alt="Clicky" width="1" height="1" src="http://static.getclicky.com/130633ns.gif" /></p></noscript>
+
 </body>
 </html>
